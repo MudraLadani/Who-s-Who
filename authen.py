62e6c659a71499381308f91af62b22b5878c73d5
Mudra Ladani <mudraladani@gmail.com>
2014-10-03 17:48:00 -0700

    successfully pulled all connections

---
+++ authen.py
@@ -0,0 +1,58 @@
+import pprint
+from linkedin import linkedin # pip install python-linkedin
+# Define CONSUMER_KEY, CONSUMER_SECRET,
+# USER_TOKEN, and USER_SECRET from the credentials
+# provided in your LinkedIn application
+CONSUMER_KEY = '75ucap9az7urp8'
+CONSUMER_SECRET = 'ZfyZ2nA1SEc8RIow'
+USER_TOKEN = '7ace1235-cc50-4c82-9155-dd3b529ae5e4'
+USER_SECRET = 'fb15ff0b-157d-4810-b905-40a53ca7a4d1'
+RETURN_URL = 'http://localhost:3000' # Not required for developer authentication
+# Instantiate the developer authentication class
+auth = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET,USER_TOKEN, USER_SECRET,RETURN_URL,permissions=linkedin.PERMISSIONS.enums.values())
+app = linkedin.LinkedInApplication(auth) # Use the app...
+app.get_profile()
+
+pp = pprint.PrettyPrinter(indent=1)
+
+print app.get_profile()
+
+profile_data = app.get_profile(selectors=['id', 'first-name', 'last-name', 'location', 'distance', 'connections' ,'num-connections', 'skills', 'educations'])
+
+location = profile_data['location']
+city = location['name']
+
+##pp.pprint(profile_data)
+connections = profile_data['connections']
+values = connections['values']
+
+for value in values:
+	##pp.pprint(value)
+	print "----- "
+	print " "
+	firstN = value['firstName']
+	lastN = value['lastName']
+	defaultVal = 'On Earth :-)'
+	place = value.get('location',defaultVal)
+	if place == defaultVal:
+		continue
+	city2 = place['name']
+	print firstN + " " + lastN 
+	print city2
+
+##pp.pprint(values)
+
+
+print city
+
+
+print " "
+print " --------------------------- "
+print "Company Info"
+updates = app.get_company_updates(1035, params={'count': 2})
+
+print type(updates)
+pp.pprint(updates)
+
+
+	
--- authentication.py
+++ authentication.py
@@ -3,10 +3,21 @@ from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
 
 
 if __name__ == '__main__':
-    API_KEY = 'wFNJekVpDCJtRPFX812pQsJee-gt0zO4X5XmG6wcfSOSlLocxodAXNMbl0_hw3Vl'
-    API_SECRET = 'daJDa6_8UcnGMw1yuq9TjoO_PMKukXMo8vEMo7Qv5J-G3SPgrAV0FqFCd0TNjQyG'
-    RETURN_URL = 'http://localhost:8000'
+    API_KEY = '75ucap9az7urp8'
+    API_SECRET = 'ZfyZ2nA1SEc8RIow'
+    RETURN_URL = 'http://localhost:3000'
     authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL,
                                             PERMISSIONS.enums.values())
     print authentication.authorization_url
     application = LinkedInApplication(authentication)
+    
+    authentication.authorization_code = 'AQSQ2RNtXTzC4r7NiV85HSE7Z1ouHrgLuErZatSIzliu2VzHjE7tPZ9f1dztSbQXCIYYyKWWH9mqzyGsJvvYHUVSOjTYFJiwd_vAe-zZ8MNNQf9Tkr8'
+    token_info = authentication.get_access_token()
+
+    print token_info
+    
+    application = LinkedInApplication(token=token_info)
+    profile_data = application.get_profile()
+    print profile_data
+
+
